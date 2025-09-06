0b54e431a9db8b7d03e8a275947936bbe3cc140e
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2025-09-06 03:22:09 -0400

    Feat: add Station model

---
+++ src/Model/Station.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stationService.src.Model
+{
+    public class Station
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string UserID { get; set; } = string.Empty;
+
+        public string NameStation { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        [RegularExpression("Origen|Destino|Intermedia", ErrorMessage = "El tipo debe ser Origen, Destino o Intermedia")]
+        public string Type { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } 
+    }
+}
