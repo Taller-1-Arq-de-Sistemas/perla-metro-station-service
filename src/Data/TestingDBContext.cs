f2bfc0a659a5b98d7f2943a8e919186d4fe33587
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2025-09-06 23:33:51 -0400

    Feat: add Testing DbContext and conecction in Program.cs

---
+++ src/Data/TestingDBContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using stationService.src.Model;
+
+namespace stationService.src.Data
+{
+    public class TestingDBContext : DbContext
+    {
+        public TestingDBContext(DbContextOptions<TestingDBContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Station> Stations { get; set; }
+    }
+}
