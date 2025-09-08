ad4cfff70354daf5e58901ed2202cbd4390b5b4f
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2025-09-07 22:48:04 -0300

    Feat: add create and response station DTO

---
+++ src/StationDto/CreateStationDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stationService.src.StationDto
+{
+    public class CreateStationDto
+    {
+
+        public string NameStation { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        [RegularExpression("Origen|Destino|Intermedia", ErrorMessage = "El tipo debe ser Origen, Destino o Intermedia")]
+        public string Type { get; set; } = string.Empty;
+
+    }
+}
---
+++ src/StationDto/ResponseStationDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stationService.src.StationDto
+{
+    public class ResponseStationDto
+    {
+         public Guid ID { get; set; }
+
+        public string NameStation { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } 
+    }
+}
